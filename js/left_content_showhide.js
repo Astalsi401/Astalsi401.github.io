1e055784f96a30b088446302d4c7670b965d4872
Astalsi401 <misting401@gmail.com>
2021-06-26 01:05:06 +0800

    update fh4 left content

--- js/left_content_showhide.js
+++ js/left_content_showhide.js
@@ -1,5 +1,5 @@
-function showhide(){
-	document.all.left_content.style.display=(document.all.left_content.style.display =='none')?'':'none'
-	document.all.right_arrow.style.display=(document.all.right_arrow.style.display =='none')?'':'none'
-	document.all.left_arrow.style.display=(document.all.left_arrow.style.display =='none')?'':'none'
-}
+$(document).ready(function(){
+  $('.left_content_button').click(function(){
+    $('.left_content').animate({width:'toggle'},350);
+  });
+});
