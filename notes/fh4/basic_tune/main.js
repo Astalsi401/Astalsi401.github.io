f284cf7e1d33cb674a0b009c0864d13a5c1253e4
Astalsi401 <misting401@gmail.com>
2023-03-21 10:18:31 +0800

    update line-height

--- notes/fh4/basic_tune/main.js
+++ notes/fh4/basic_tune/main.js
@@ -42,39 +42,27 @@ class Content extends React.Component {
     return (
       <div>
         <section className="my-4">
-          <p>
-            本頁面為Youtube上FH4車輛調教影片的簡易中文摘要。
-            <br />
-            影片清單：
-            <ul></ul>
-            <ul>
+          本頁面為Youtube上FH4車輛調教影片的簡易中文摘要。
+          <br />
+          影片清單：
+          <ul>
+            {this.data.videos.map((creator) => (
               <li>
-                <a href="https://www.youtube.com/c/HokiHoshi">HokiHoshi</a>
+                <a href={creator.channel} target="_blank">
+                  {creator.creator}
+                </a>
+                <ul>
+                  {creator.links.map((link) => (
+                    <li>
+                      <a href={link.href} target="_blank">
+                        {link.title}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
               </li>
-              <ul>
-                <li>
-                  <a href="https://youtu.be/WM7_3NGGUoQ">Forza Horizon 4 Tuning Guide</a>
-                </li>
-                <li>
-                  <a href="https://youtu.be/D6qthLir2iI">Gear Tuning</a>
-                </li>
-                <li>
-                  <a href="https://youtu.be/BfZHOWwR5Gw">Drift Guide</a>
-                </li>
-                <li>
-                  <a href="https://youtu.be/rAuvIlPi2KE">Drift Gearing</a>
-                </li>
-              </ul>
-              <li>
-                <a href="https://www.youtube.com/channel/UCGK33hhvffYv5hUNqB0wVnQ">Johnson Racing</a>
-              </li>
-              <ul>
-                <li>
-                  <a href="https://youtu.be/W4s1VP1nUoM">HOW TO TUNE in Forza Horizon 4 | OP Car Tutorial (Upgrades & Tuning)</a>
-                </li>
-              </ul>
-            </ul>
-          </p>
+            ))}
+          </ul>
         </section>
       </div>
     );
