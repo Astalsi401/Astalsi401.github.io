64d26236a235e0fe4b926e486747a683d7a00245
Astalsi401 <misting401@gmail.com>
2023-05-02 09:13:39 +0800

    update index.json

--- portfolio/javascript-d3js-treemap/render.js
+++ portfolio/javascript-d3js-treemap/render.js
@@ -1,1 +1,2 @@
 ReactDOM.createRoot(document.getElementById("App-header")).render(<Header category="Portfolio" />);
+ReactDOM.createRoot(document.getElementById("code-1")).render(<CodeChunkFromFile path="./graph/js/treemap.js" lang="js" />);
