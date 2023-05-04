32ef7cc07c74f703810b7492b01b24a894caa0a8
Astalsi401 <misting401@gmail.com>
2023-05-04 11:26:52 +0800

    accessibility: copyBtn

--- assets/js/component/components.js
+++ assets/js/component/components.js
@@ -125,7 +125,8 @@ class CodeChunk extends React.Component {
   componentDidMount() {
     Prism.highlightAll();
   }
-  copy = () => {
+  copy = (e) => {
+    e.preventDefault();
     navigator.clipboard.writeText(this.props.code);
     this.setState({ active: true });
     setTimeout(() => {
@@ -135,12 +136,12 @@ class CodeChunk extends React.Component {
   render() {
     return (
       <pre>
-        <div className={`copyBtn ${toggleActive(this.state.active)}`} onClick={this.copy}>
+        <a href="#" className={`copyBtn ${toggleActive(this.state.active)}`} onClick={this.copy}>
           <svg viewBox="0 0 10 10">
             <path fill="none" d="M4.5 1 L7.5 1 Q8.5 1,8.5 2 L8.5 6 Q8.5 7,7.5 7 L4.5 7 Q3.5 7,3.5 6 L3.5 2 Q3.5 1,4.5 1"></path>
             <path fill="none" d="M2.5 3 L5.5 3 Q6.5 3,6.5 4 L6.5 8 Q6.5 9,5.5 9 L2.5 9 Q1.5 9,1.5 8 L1.5 4 Q1.5 3,2.5 3"></path>
           </svg>
-        </div>
+        </a>
         <code children={this.props.code} className={`lang-${this.props.language}`} />
       </pre>
     );
