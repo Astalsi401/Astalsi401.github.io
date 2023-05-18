d42dc42d1154279c8ca802b3d2bb7f7f5703d035
Astalsi401 <misting401@gmail.com>
2023-05-18 15:18:38 +0800

    update portfolio index page hover effect

--- portfolio/main.js
+++ portfolio/main.js
@@ -7,7 +7,18 @@ class Portfolio extends React.Component {
       css: "https://astalsi401.github.io/assets/images/CSS3-logo.svg",
       js: "https://astalsi401.github.io/assets/images/js-logo.svg",
     };
+    this.ref = React.createRef();
   }
+  handleMouseEnter = (e) => {
+    const { height } = e.currentTarget.getBoundingClientRect();
+    if (e.currentTarget.querySelector(".page-view")) {
+      e.currentTarget.querySelector(".page-view").style.transform = `translateY(calc(-100% + ${height}px))`;
+      e.currentTarget.querySelector(".page-view").style.transition = "4s";
+    }
+  };
+  handleMouseLeave = (e) => {
+    if (e.currentTarget.querySelector(".page-view")) e.currentTarget.querySelector(".page-view").style.transform = `translateY(0)`;
+  };
   componentDidMount() {
     fetch("https://astalsi401.github.io/assets/js/json/index.json")
       .then((res) => res.json())
@@ -25,13 +36,14 @@ class Portfolio extends React.Component {
           {this.state.index.pages.map((page) => (
             <div className="col-sm-6 col-md-4 col-lg-3 p-2" key={page.page}>
               <a className="d-block bg-white shadow-sm w-100 h-100 text-center text-decoration-none portfolio" href={page.href} target="_blank">
-                <div className="w-100 ratio-16by9 portfolioThumb overflow-hidden">
+                <div className={`w-100 ratio-16by9 portfolioThumb overflow-hidden ${this.logo[page.thumbnail] ? "page-logo" : "page-view"}`} style={{ backgroundImage: `url(${this.logo[page.thumbnail] ? this.logo[page.thumbnail] : page.thumbnail})` }}>
                   <div className="tags">
-                    {page.tags.map((tag) => (
-                      <div className="tag text-small px-1 m-1 rounded-1">{tag}</div>
+                    {page.tags.map((tag, i) => (
+                      <div key={tag} className="tag text-small px-1 m-1 rounded-1 shadow-sm" style={{ "--i": i }}>
+                        {tag}
+                      </div>
                     ))}
                   </div>
-                  <img className={this.logo[page.thumbnail] ? "w-100 h-100 object-fit-cover" : "w-100"} src={this.logo[page.thumbnail] ? this.logo[page.thumbnail] : page.thumbnail} alt={page.page} />
                 </div>
                 <div className="py-2">{page.page}</div>
               </a>
