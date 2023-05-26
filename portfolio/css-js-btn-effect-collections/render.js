be52987909fc86e9cce3789766885a65538a1a78
Astalsi401 <misting401@gmail.com>
2023-05-26 09:37:35 +0800

    add page btn effects

--- portfolio/css-js-btn-effect-collections/render.js
+++ portfolio/css-js-btn-effect-collections/render.js
@@ -1,1 +1,15 @@
-ReactDOM.createRoot(document.getElementById("App-header")).render(<Header category="Portfolio" />);
+const contents = [
+  {
+    id: "App-header",
+    content: <Header category="Portfolio" />,
+  },
+  {
+    id: "btn1-html",
+    content: <CodeChunk code={`<a class="btn1">btn1\n    <span style="--i:0;"></span>\n    <span style="--i:0;"></span>\n    <span style="--i:1;"></span>\n    <span style="--i:2;"></span>\n</a>`} lang="html" />,
+  },
+  {
+    id: "btn1-scss",
+    content: <CodeChunkFromFile path="./btn1.scss" lang="scss" />,
+  },
+];
+contents.forEach((item) => ReactDOM.createRoot(document.getElementById(item.id)).render(item.content));
