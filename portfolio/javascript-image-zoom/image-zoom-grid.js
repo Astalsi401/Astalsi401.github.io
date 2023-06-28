814038be9d6661263bcb27cb6b50f9d4367d6a74
Astalsi401 <misting401@gmail.com>
2023-06-28 11:36:06 +0800

    update image zoom on click: add grid version

---
+++ portfolio/javascript-image-zoom/image-zoom-grid.js
@@ -0,0 +1,21 @@
+class GridImageZoom {
+  constructor(img) {
+    this.img = img;
+    this.active = false;
+    document.addEventListener("click", this.zoom);
+  }
+  zoom = ({ target }) => {
+    this.active = target === this.img ? !this.active : false;
+    this.img.classList.toggle("active", this.active);
+    if (this.active) {
+      this.img.style.zIndex = 101;
+    } else {
+      setTimeout(() => (this.img.style.zIndex = 1), 800);
+    }
+    const { x, y, width, height } = this.img.getBoundingClientRect();
+    const scale = Math.min(window.innerWidth / width, window.innerHeight / height);
+    const translate = `${(window.innerWidth / 2 - x) / scale - width / 2}px, ${(window.innerHeight / 2 - y) / scale - height / 2}px`;
+    this.img.style.transform = this.active ? `scale(${scale}) translate(${translate})` : "scale(1) translate(0)";
+  };
+}
+document.querySelectorAll(".grid-image-zoom").forEach((img) => new GridImageZoom(img));
