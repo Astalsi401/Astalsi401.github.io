68ea4797efc9a8b80496dd5913f77092de1db93c
Astalsi401 <misting401@gmail.com>
2023-07-19 17:35:58 +0800

    add page basic chart

--- portfolio/javascript-d3js-basic-chart/graph/js/非主管員工薪資-main.js
+++ portfolio/javascript-d3js-basic-chart/graph/js/非主管員工薪資-main.js
@@ -1,7 +1,7 @@
 fetch("https://astalsi401.github.io/warehouse/ibmi/2022非主管員工薪資.json")
   .then((res) => res.json())
   .then((data) => {
-    new PlotGraph("#graph-plot", data.plot, "en");
+    new PlotGraph("#graph-plot1", data.plot, "en");
     new BarGraph(
       "#graph-bar1",
       data.bar.filter((d) => d.market === "上市"),
