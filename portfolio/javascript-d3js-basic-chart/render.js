ee7139be92c0df3f8b55a80f12db2b3f9c1c65b3
Astalsi401 <misting401@gmail.com>
2023-07-20 08:46:41 +0800

    update page basic chart

--- portfolio/javascript-d3js-basic-chart/render.js
+++ portfolio/javascript-d3js-basic-chart/render.js
@@ -1,3 +1,3 @@
 ReactDOM.createRoot(document.getElementById("App-header")).render(<Header category="Portfolio" />);
-ReactDOM.createRoot(document.getElementById("graph-plot1")).render(<CodeChunkFromFile path="./graph/js/非主管員工薪資-plot.js" lang="js" />);
-ReactDOM.createRoot(document.getElementById("graph-bar1")).render(<CodeChunkFromFile path="./graph/js/非主管員工薪資-bar.js" lang="js" />);
+ReactDOM.createRoot(document.getElementById("code-plot1")).render(<CodeChunkFromFile path="./graph/js/非主管員工薪資-plot.js" lang="js" />);
+ReactDOM.createRoot(document.getElementById("code-bar1")).render(<CodeChunkFromFile path="./graph/js/非主管員工薪資-bar.js" lang="js" />);
