2f7ce986d2042672d3debb6a70b679b610eb8deb
Astalsi401 <misting401@gmail.com>
2023-08-04 14:25:13 +0800

    update portfolio floormap

--- warehouse/show/floormap-render.js
+++ warehouse/show/floormap-render.js
@@ -1,1 +1,1 @@
-ReactDOM.createRoot(document.getElementById("App")).render(<App category="Portfolio" title="南港展覽館醫療科技展平面圖" className="container-xl shadow-lg p-3" />);
+ReactDOM.createRoot(document.getElementById("App")).render(<FloormapArea />);
