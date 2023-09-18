575ae141709462ccfc63c3197f2e96a7a838082f
Astalsi401 <misting401@gmail.com>
2023-09-18 09:32:05 +0800

    edit show/news main.js

--- warehouse/show/news/main.js
+++ warehouse/show/news/main.js
@@ -14,5 +14,11 @@ const swiperAction = ({ currentTarget }) => {
   getSiblings(newActive.previousElementSibling, container.lastElementChild).classList.add("prev");
   getSiblings(newActive.nextElementSibling, container.firstElementChild).classList.add("next");
 };
-document.querySelectorAll(".swiper").forEach((swiper) => swiper.querySelectorAll(".swiper-next, .swiper-prev").forEach((btn) => btn.addEventListener("click", swiperAction)));
+const autoSwiper = () => document.querySelectorAll(".swiper").forEach((swiper) => swiper.querySelector(".swiper-next").click());
+let swiperTimer = setInterval(autoSwiper, 7000);
+document.querySelectorAll(".swiper").forEach((swiper) => {
+  swiper.querySelectorAll(".swiper-next, .swiper-prev").forEach((btn) => btn.addEventListener("click", swiperAction));
+  swiper.addEventListener("mouseenter", () => clearInterval(swiperTimer));
+  swiper.addEventListener("mouseleave", () => (swiperTimer = setInterval(autoSwiper, 7000)));
+});
 document.querySelectorAll(".intersection").forEach((elem) => observer.observe(elem));
