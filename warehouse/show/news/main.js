4ab54d4b81b08c87a2511f2ac45ceb8494d5e880
Astalsi401 <misting401@gmail.com>
2023-09-21 09:57:44 +0800

    update floormap

--- warehouse/show/news/main.js
+++ warehouse/show/news/main.js
@@ -1,4 +1,6 @@
 const observer = new IntersectionObserver((entries) => entries.forEach((entry) => entry.isIntersecting && entry.target.classList.add("show"), { threshold: 1 }));
+const observer2 = new IntersectionObserver((entries) => entries.forEach((entry) => entry.isIntersecting && entry.target.classList.toggle("show"), { threshold: 1 }));
+const swipers = document.querySelectorAll(".swiper.show");
 const getSiblings = (elem, defaultElem) => (elem ? elem : defaultElem);
 const swiperAction = ({ currentTarget }) => {
   const nextClick = currentTarget.classList.contains("swiper-next");
@@ -14,11 +16,12 @@ const swiperAction = ({ currentTarget }) => {
   getSiblings(newActive.previousElementSibling, container.lastElementChild).classList.add("prev");
   getSiblings(newActive.nextElementSibling, container.firstElementChild).classList.add("next");
 };
-const autoSwiper = () => document.querySelectorAll(".swiper").forEach((swiper) => swiper.querySelector(".swiper-next").click());
+const autoSwiper = () => swipers.forEach((swiper) => swiper.querySelector(".swiper-next").click());
 let swiperTimer = setInterval(autoSwiper, 7000);
-document.querySelectorAll(".swiper").forEach((swiper) => {
+swipers.forEach((swiper) => {
   swiper.querySelectorAll(".swiper-next, .swiper-prev").forEach((btn) => btn.addEventListener("click", swiperAction));
   swiper.addEventListener("mouseenter", () => clearInterval(swiperTimer));
   swiper.addEventListener("mouseleave", () => (swiperTimer = setInterval(autoSwiper, 7000)));
 });
 document.querySelectorAll(".intersection").forEach((elem) => observer.observe(elem));
+document.querySelectorAll(".intersection2").forEach((elem) => observer2.observe(elem));
