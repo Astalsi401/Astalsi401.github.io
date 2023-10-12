3ad66bfcd1a1282454b5a7f27c25f0815aee8904
Astalsi401 <misting401@gmail.com>
2023-10-12 12:04:51 +0800

    add show news main.js

--- warehouse/show/news/main.js
+++ warehouse/show/news/main.js
@@ -1,22 +1,26 @@
 const observer = new IntersectionObserver((entries) => entries.forEach((entry) => entry.isIntersecting && entry.target.classList.add("show"), { threshold: 1 }));
 const observer2 = new IntersectionObserver((entries) => entries.forEach((entry) => entry.target.classList.toggle("show", entry.isIntersecting), { threshold: 1 }));
 const swipers = document.querySelectorAll(".swiper");
-const getSiblings = (elem, defaultElem) => (elem ? elem : defaultElem);
+let i = 0;
 const swiperAction = ({ currentTarget }) => {
-  const nextClick = currentTarget.classList.contains("swiper-next");
-  const container = currentTarget.parentElement.querySelector(".swiper-container");
-  const cardActive = currentTarget.parentElement.querySelector(".swiper-card.active");
-  const prev = getSiblings(cardActive.previousElementSibling, container.lastElementChild);
-  const next = getSiblings(cardActive.nextElementSibling, container.firstElementChild);
-  cardActive.classList.remove("active");
-  next.classList.remove("next");
-  prev.classList.remove("prev");
-  const newActive = nextClick ? next : prev;
-  newActive.classList.add("active");
-  getSiblings(newActive.previousElementSibling, container.lastElementChild).classList.add("prev");
-  getSiblings(newActive.nextElementSibling, container.firstElementChild).classList.add("next");
+  const container = currentTarget.parentElement.querySelector(".swiper-container").style;
+  let w = container.getPropertyValue("--w");
+  let min = -(container.getPropertyValue("--n") - w),
+    max = 0;
+  console.log(currentTarget.parentElement.querySelector(".swiper-container"));
+  i += currentTarget.classList.contains("swiper-next") ? -1 : 1;
+  i = i < min ? max : i > max ? min : i;
+  container.setProperty("--i", i);
 };
 const autoSwiper = () => swipers.forEach((swiper) => swiper.classList.contains("show") && swiper.querySelector(".swiper-next").click());
+const swiperSetting = () => {
+  swipers.forEach((swiper) => {
+    const w = window.innerWidth < 576 ? 1 : window.innerWidth < 768 ? 2 : 4;
+    const containerStyle = swiper.querySelector(".swiper-container").style;
+    containerStyle.setProperty("--w", w);
+    containerStyle.setProperty("--n", swiper.querySelector(".swiper-container").childElementCount);
+  });
+};
 let swiperTimer = setInterval(autoSwiper, 10000);
 swipers.forEach((swiper) => {
   swiper.querySelectorAll(".swiper-next, .swiper-prev").forEach((btn) => btn.addEventListener("click", swiperAction));
@@ -25,3 +29,5 @@ swipers.forEach((swiper) => {
 });
 document.querySelectorAll(".intersection").forEach((elem) => observer.observe(elem));
 document.querySelectorAll(".intersection2").forEach((elem) => observer2.observe(elem));
+swiperSetting();
+window.addEventListener("resize", swiperSetting);
