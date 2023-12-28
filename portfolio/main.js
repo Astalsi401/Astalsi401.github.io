65696add89af8d789d3eaf10ef23ebbc6dd0c983
Astalsi401 <misting401@gmail.com>
2023-12-28 11:46:48 +0800

    update protfolio page

--- portfolio/main.js
+++ portfolio/main.js
@@ -1,57 +1,30 @@
-class Portfolio extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { index: {}, indexLoaded: false };
-    this.logo = {
-      python: "https://astalsi401.github.io/assets/images/python-logo.svg",
-      css: "https://astalsi401.github.io/assets/images/CSS3-logo.svg",
-      js: "https://astalsi401.github.io/assets/images/js-logo.svg",
-    };
-    this.ref = React.createRef();
-  }
-  componentDidMount() {
-    fetch("https://astalsi401.github.io/assets/js/json/index.json")
-      .then((res) => res.json())
-      .then((data) => {
-        this.setState({
-          index: data.index.find((d) => d.category === this.props.category),
-          indexLoaded: true,
-        });
-      });
-  }
-  render() {
-    if (this.state.indexLoaded) {
-      return (
-        <div className="row">
-          {this.state.index.pages.map((page) => (
-            <div className="col-sm-6 col-md-4 col-lg-3 p-2" key={page.page}>
-              <a className="d-block bg-white shadow-sm w-100 h-100 text-center text-decoration-none portfolio" href={page.href} target="_blank">
-                <div className={`w-100 ratio-16by9 portfolioThumb overflow-hidden ${this.logo[page.thumbnail] ? "page-logo" : "page-view"}`} style={{ backgroundImage: `url(${this.logo[page.thumbnail] ? this.logo[page.thumbnail] : page.thumbnail})` }}>
-                  <div className="tags">
-                    {page.tags.map((tag, i) => (
-                      <div key={tag} className="tag text-small px-1 m-1 rounded-1 shadow-sm" style={{ "--i": i }}>
-                        {tag}
-                      </div>
-                    ))}
-                  </div>
+const Content = () => {
+  const logo = {
+    python: "https://astalsi401.github.io/assets/images/python-logo.svg",
+    css: "https://astalsi401.github.io/assets/images/CSS3-logo.svg",
+    js: "https://astalsi401.github.io/assets/images/js-logo.svg",
+  };
+  const { index, indexLoaded } = useIndexData("Portfolio");
+  if (indexLoaded) {
+    return (
+      <div className="row">
+        {index.pages.map((page) => (
+          <div className="col-sm-6 col-md-4 col-lg-3 p-2" key={page.page}>
+            <a className="d-block bg-white shadow-sm w-100 h-100 text-center text-decoration-none portfolio" href={page.href} target="_blank">
+              <div className={`w-100 ratio-16by9 portfolioThumb overflow-hidden ${logo[page.thumbnail] ? "page-logo" : "page-view"}`} style={{ backgroundImage: `url(${logo[page.thumbnail] ? logo[page.thumbnail] : page.thumbnail})` }}>
+                <div className="tags">
+                  {page.tags.map((tag, i) => (
+                    <div key={tag} className="tag text-small px-1 m-1 rounded-1 shadow-sm" style={{ "--i": i }}>
+                      {tag}
+                    </div>
+                  ))}
                 </div>
-                <div className="py-2">{page.page}</div>
-              </a>
-            </div>
-          ))}
-        </div>
-      );
-    }
+              </div>
+              <div className="py-2">{page.page}</div>
+            </a>
+          </div>
+        ))}
+      </div>
+    );
   }
-}
-
-class Content extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { index: {}, indexLoaded: true };
-  }
-
-  render() {
-    return <Portfolio category="Portfolio" />;
-  }
-}
+};
