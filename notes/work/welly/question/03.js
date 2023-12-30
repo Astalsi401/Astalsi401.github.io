cd4815793f04d5c7391c2705a7d0786566b8c6c3
Astalsi401 <misting401@gmail.com>
2023-12-30 16:02:32 +0800

    update 03.js

--- notes/work/welly/question/03.js
+++ notes/work/welly/question/03.js
@@ -4,7 +4,7 @@ function formatName(firstName = "", lastName = "") {
     // 如果不是字串就拋出錯誤
     throw new TypeError("firstName and lastName must be strings");
   }
-  // 用空格將名字合併，並回傳
-  // 過濾空白參數，使用join避免只輸入firstName或lastName之一時出現不必要的空格
-  return [firstName, lastName].filter((n) => n.length > 0).join(" ");
+  // 用空格將名字合併
+  // 刪除頭尾空格，避免只輸入firstName或lastName之一時出現不必要的空格
+  return [firstName, lastName].join(" ").trim();
 }
