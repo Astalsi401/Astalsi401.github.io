565d86dfd646558e30d1f96d1dc9e0f7639e39ef
Astalsi401 <misting401@gmail.com>
2024-01-22 17:08:15 +0800

    update frame.js

--- assets/js/frame.js
+++ assets/js/frame.js
@@ -1,1 +1,5 @@
 window.addEventListener("click", () => parent.postMessage({ window: "iframe" }));
+const resizeObserver = new ResizeObserver((entries) => {
+  for (let entry of entries) parent.postMessage({ height: entry.contentRect.height });
+});
+resizeObserver.observe(document.body);
